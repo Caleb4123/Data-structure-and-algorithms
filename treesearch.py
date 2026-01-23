7f48c7ed69aa2fa7899413a50b626941abaa3cbb
Caleb4123 <caleb10eq@gmail.com>
2026-01-23 17:39:00 +0000

    Add files via upload

--- treesearch.py
+++ treesearch.py
@@ -30,7 +30,43 @@ def search(root,item):
         return search(root.right,item)
     else:
         return -1
+#function for minimum value node
 
+def minimum(root):
+    mini = root
+    while mini.left != None:
+        mini = mini.left
+    return mini
+
+
+def delete(root,item):
+    if root is None:
+        return root
+    elif item < root.data:
+        root.left = delete(root.left,item)
+    elif item > root.data:
+        root.right = delete(root.right,item)
+    else:
+        if root.left is None and root.right is None:
+            return None
+            #root has only 1 child
+        elif root.left is None:
+            var = root.right 
+            #root = None
+            return var
+        elif root.right is None:
+            var = root.right
+            #root = None
+            return var
+        #if root has 2 child
+        #else:
+        var = minimum(root.right)
+        #print(var.data)
+        #temp = root.data
+        root.data = var.data
+        #var.data = temp
+        root.right = delete(root.right,var.data)
+    return root
 #number of items
 num = int(input("How many items are in the tree? "))
 root = None
@@ -40,13 +76,16 @@ for i in range(num):
 inorder(root)
 
 searched = int(input("Enter the item to be searched for: "))
+print(searched,"is on level")
 value2 = search(root,searched)
 if value2 == -1:
     print("The item is not in the tree")
 else:
     print("The item is present in the tree")
 
+user = int(input("Enter the number to delete: "))
+var2 = delete(root,user)
 
-
-
+print("tree after deletion")
+inorder(root)
 
